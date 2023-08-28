8db4017c25785d1f0c3aacef3f064f2d2261b932
abdul-aleem23 <abdul_aleem98@hotmail.com>
2023-08-28 17:01:26 +0200

    Fixed engines

--- data/database.js
+++ data/database.js
@@ -5,6 +5,6 @@ export const connectDB = () => {
     .connect(process.env.MONGO_URI, {
         dbname: "backendapi",
     })
-    .then(() => console.log("Database status: Connected"))
+    .then((c) => console.log(`Database status: Connected, with: ${c.connection.host}`))
     .catch((e) => console.log(`Database disconnected: ${e}`));
 }
